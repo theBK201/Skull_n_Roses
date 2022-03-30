cf56052418fa8c7e2d23737010983f56d313b05b
bk201 <bk201.dev@gmail.com>
2022-03-30 11:35:37 +0200

    initial commit

---
+++ gameBoard.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 900,
+    height: 700,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    
+}
+
+function create ()
+{
+    
+}
+
+function update ()
+{
+}

e91fdd439a04576f73238b0bf59a8d588e3f4119
bk201 <bk201.dev@gmail.com>
2022-04-15 20:20:47 +0200

    The game mats and coasters are now taken from spritesheet, added button class which i can call wherever i want

--- public/gameBoard.js
+++ public/gameBoard.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
     width: 1300,
-    height: 820,
+    height: 860,
     parent: 'tableDiv',
     physics: {
         default: 'arcade',
@@ -17,7 +17,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-let coaster1;
+var coaster1;
 let coaster2;
 let coaster3;
 let mat1;
@@ -31,23 +31,19 @@ var clickedDisk = false;
 var diskPlaced = false;
 var infoBox;
 var infoBoxText;
+let passButton;
 
 function preload ()
 {
-    // this.load.spritesheet('mats', '/assets/mats.png', { frameWidth: 270, frameHeight: 270, endFrame: 5 })
-    // this.load.spritesheet('coasters', '/assets/coasters.png', { frameWidth: 250, frameHeight: 250, endFrame: 5 })
-    this.load.image('coaster1', 'assets/Coasters/coaster1.1.png')
-    this.load.image('coaster1_2', 'assets/Coasters/coaster1.2.png')
-    this.load.image('coaster1_3', 'assets/Coasters/coaster1.3.png')
-    this.load.image('mat1', 'assets/Mats/mat1.png')
-    this.load.image('mat2', 'assets/Mats/mat2.png') 
-    this.load.image('mat3', 'assets/Mats/mat3.png') 
+    this.load.spritesheet('mats', '/assets/mats.png', { frameWidth: 280, frameHeight: 280})
+    this.load.spritesheet('coasters', '/assets/coasters.png', { frameWidth: 250, frameHeight: 250})
     this.load.image('gameBackground', 'assets/gameBackground.png')
     this.load.image('dialogBox', 'assets/dialog_box.png')
 }
 
 function create ()
 {
+    this.socket = io();
     // var config = {
     //     key: 'spiningAnimation',
     //     frames : this.anims.generateFrameNumbers('spiningAnimation', { start: 0, end: 5, first: 5 }),
@@ -59,7 +55,7 @@ function create ()
     // this.add.sprite(200,200,'mats');
     // this.add.sprite(400,300, 'coasters').play(spiningAnimation);
 
-    var gameBackground = this.add.image(645,410,'gameBackground');
+    var gameBackground = this.add.image(645,445,'gameBackground');
     gameBackground.setScale(0.6);
     // var infoDom = this.add.dom(1110,120, 'div', infoBoxStyle, 'Sorry, you have already placed a disk on your mat.')
     infoBox = this.add.image(1110,120,'dialogBox')
@@ -71,81 +67,53 @@ function create ()
     infoBoxText.visible = false;
   
 
-    // var infoBoxStyle = {
-    //     'width': '20px',
-    //     'height': '20px',
-    //     'font': '10px Arial'
-    // };
+    var graphics = this.make.graphics().fillStyle(0x612c0c).fillRect(0, 0, 1400, 40);
 
-    timerText = this.add.text(1100,45,'Timer: 0', { fontSize: '16px'});
+    graphics.generateTexture('hudbar', 1400, 40);
 
-    mat1 = this.add.image(400,250,'mat1')
-    mat1.setScale(0.6)
-    mat2 = this.add.image(650,600,'mat2')
-    mat2.setScale(0.6)
-    mat3 = this.add.image(900,250,'mat3')
-    mat3.setScale(0.6)
+    graphics.destroy();
 
-    const player1 = this.add.text(620,700, 'Player 1');
-    const player2 = this.add.text(360,350, 'Player 2');
-    const player3 = this.add.text(860,350, 'Player 3');
-    
+    this.add.image(600, 10, 'hudbar');
 
+    timerText = this.add.text(1100,80,'Timer: 0', { fontSize: '16px'});
 
-    coaster1 = this.add.sprite(500,680,'coaster1_2').setInteractive();
-    coaster2 = this.add.sprite(480,680,'coaster1_2').setInteractive();
-    coaster3 = this.add.sprite(460,680,'coaster1_2').setInteractive();
-    skullCoaster1 = this.add.sprite(800,680,'coaster1_3').setInteractive();
+    mat1 = this.add.sprite(400,280,'mats', 6);
+    mat1.setScale(0.6);
+    mat1.setAngle(40);
+    mat2 = this.add.sprite(650,620,'mats', 7);
+    mat2.setScale(0.6);
+    mat3 = this.add.sprite(900,280,'mats', 8);
+    mat3.setScale(0.6);
+    mat3.setAngle(55);
+
+    var player1 = this.add.text(620,720, 'Player 1');
+    var player2 = this.add.text(290,240, 'Player 2');
+    player2.setAngle(310);
+    var player3 = this.add.text(965,180, 'Player 3');
+    player3.setAngle(58);
+
+    //Player 1
+    coaster1 = this.add.sprite(500,680,'coasters', 6).setInteractive();
+    coaster2 = this.add.sprite(480,680,'coasters', 6).setInteractive();
+    coaster3 = this.add.sprite(460,680,'coasters', 6).setInteractive();
+    skullCoaster1 = this.add.sprite(800,680,'coasters', 12).setInteractive();
     coaster1.setScale(0.5);
     coaster2.setScale(0.5);
     coaster3.setScale(0.5);
     skullCoaster1.setScale(0.5);
-
-    coaster4 = this.add.sprite(200,280,'coaster1').setInteractive();
-    coaster5 = this.add.sprite(220,280,'coaster1').setInteractive();
-    coaster6 = this.add.sprite(240,280,'coaster1').setInteractive();
-    skullCoaster2 = this.add.sprite(550,280,'coaster1_3').setInteractive();
-    coaster4.setScale(0.5);
-    coaster5.setScale(0.5);
-    coaster6.setScale(0.5);
-    skullCoaster2.setScale(0.5);
-
-    this.input.setDraggable(coaster4);
-    this.input.setDraggable(coaster5);
-    this.input.setDraggable(coaster6);
-    this.input.setDraggable(skullCoaster2);
-
-    coaster7 = this.add.sprite(1050,280,'coaster1').setInteractive();
-    coaster8 = this.add.sprite(1070,280,'coaster1').setInteractive();
-    coaster9 = this.add.sprite(1090,280,'coaster1').setInteractive();
-    skullCoaster3 = this.add.sprite(750,280,'coaster1_3').setInteractive();
-    coaster7.setScale(0.5);
-    coaster8.setScale(0.5);
-    coaster9.setScale(0.5);
-    skullCoaster3.setScale(0.5);
-
-    this.input.setDraggable(coaster7);
-    this.input.setDraggable(coaster8);
-    this.input.setDraggable(coaster9);
-    this.input.setDraggable(skullCoaster3);
-
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY){
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-    });
 
     inteval = setInterval(function(){
         timer += 1;
         timerText.setText("Timer: " + timer);
     }, 1000);
 
-    coaster3.on('pointerdown', function (pointer){
-        clickedDisk = true;
-        if(diskPlaced == true){
-            infoBox.visible = true;
-            infoBoxText.visible = true;
-        }
-    } );
+    const passButton = new Button(600,13, 'Pass', this, () => console.log('Player: ' + player1.text + ' passed.'))
+    const bidOne = new Button(670,13, '1', this, () => console.log('Player: ' + player1.text + ' bids 1 disc(s).'))
+    const bidTwo = new Button(720,13, '2', this, () => console.log('Player: ' + player1.text + ' bids 2 disc(s).'))
+    const bidThree = new Button(770,13, '3', this, () => console.log('Player: ' + player1.text + ' bids 3 disc(s).'))
+    const bidFour = new Button(820,13, '4', this, () => console.log('Player: ' + player1.text + ' bids 4 disc(s).'))
+
+    // passButton = document.getElementById('passButtonID').setInteractive();
     
 }
 
@@ -153,11 +121,18 @@ function update ()
 {
     if(clickedDisk == true){
         coaster3.x = 650;
-        coaster3.y = 600;
+        coaster3.y = 620;
         diskPlaced = true;
     }
 
-    setInterval(hideInfoBox, 4000);
+    coaster3.on('pointerdown', function (pointer){
+        clickedDisk = true;
+        if(diskPlaced == true){
+            infoBox.visible = true;
+            infoBoxText.visible = true;
+            setInterval(hideInfoBox, 6000);
+        }
+    } );
 
 }
 
@@ -169,5 +144,16 @@ function hideInfoBox(){
         if(infoBox.visible == true){
             infoBox.visible = false;
             infoBoxText.visible = false;
+    }
+}
+
+class Button{
+    constructor(x, y, label, scene, callback){
+        const button = scene.add.text(x, y, label)
+            .setOrigin(0.5)
+            .setPadding(10)
+            .setStyle({backgroundColor: '#197faf'})
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => callback())
     }
 }
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var players = {};
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket){
+    console.log('a user has connected');
+    players[socket.id] = {
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) ? 'red' : 'blue' )
+    };
+
+    // // send the players object to the new player
+    // socket.emit('currentPlayers', players);
+    // // update all other players of the new player
+    // socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('disconnect', function(){
+        // // remove this player from our players object
+        // delete players[socket.id];
+        // // emit a message to all players to remove this player
+        // io.emit('disconnect', socket.id);
+        console.log('user has disconnected');
+    });
 });
 
 server.listen(8082, function(){
